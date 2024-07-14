cd80781dd597c59dab64477093790f6949543261
Lucas Buhl-Nielsen <lucasbnielsen@hotmail.com>
2024-07-14 16:10:17 +0100

    Handle dbinfo command in separate function

--- app/main.go
+++ app/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	// "github.com/xwb1989/sqlparser"
 )
 
 // Usage: sqlite3.sh sample.db .dbinfo
@@ -13,34 +12,32 @@ func main() {
 	databaseFilePath := os.Args[1]
 	command := os.Args[2]
 
+	// Open the database file and defer its closing
 	databaseFile, err := os.Open(databaseFilePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer databaseFile.Close()
 
+	// Read the database header
+	var header DatabaseHeader
+	if err := binary.Read(databaseFile, binary.BigEndian, &header); err != nil {
+		fmt.Println("Failed to read integer:", err)
+		return
+	}
+
 	switch command {
 	case ".dbinfo":
-		// Read the database header
-		var header DatabaseHeader
-		if err := binary.Read(databaseFile, binary.BigEndian, &header); err != nil {
-			fmt.Println("Failed to read integer:", err)
-			return
-		}
-
-		// Print the page size
-		fmt.Printf("database page size: %v\n", header.PageSize)
-
-		// Print the text encoding size
-		fmt.Printf("database text encoding: %v\n", header.TextEncoding)
-
-		schema := readSQLiteSchema(databaseFile, header)
-
-		// Print the number of tables
-		fmt.Printf("number of tables: %v\n", schema.TableCount())
-
+		dbinfo(databaseFile, header)
 	default:
 		fmt.Println("Unknown command", command)
 		os.Exit(1)
 	}
+}
+
+func dbinfo(databaseFile *os.File, header DatabaseHeader) {
+	schema := readSQLiteSchema(databaseFile, header)
+
+	fmt.Printf("database page size: %v\n", header.PageSize)
+	fmt.Printf("number of tables: %v\n", schema.TableCount())
 }
