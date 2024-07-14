78790819ced0adb4ec90a042c602e552f4c23161
Lucas Buhl-Nielsen <lucasbnielsen@hotmail.com>
2024-07-14 18:38:17 +0100

    Create readTable function inside table.go
    Reading the `sqlite_schema` table is actually no
    different to how we read any other table, and
    doing this allows us to read other tables as well
    provided we have the root page for them.
    
    I've also introduced a new idea of 'raw records'
    which basically is the entire byte slice of a
    record, rather than just the bytes which are
    defined directly within a cell. I think this is
    more what I wanted to do when I originally wrote
    the `cell.go` file. It allows us to detach the
    idea of pages / cells from the idea of records,
    which I think is good!

--- app/schema.go
+++ app/schema.go
@@ -16,49 +16,19 @@ type SQLiteSchema struct {
 	Rows []SQLiteSchemaTuple
 }
 
-// readSQLiteSchema does the following:
-// - Reads the first page of the database file
-// - If the page is a leaf page:
-//   - Iterates over each cell and collects the entire cell payload, which may
-//     involve reading overflow pages (currently unsupported)
-//   - Decodes the cell payload into a record format
-//
-// - If the page is an interior page:
-//   - Currently unsupported
 func readSQLiteSchema(databaseFile *os.File, dbHeader DatabaseHeader) SQLiteSchema {
-	// Read the first page, which is always a table b-tree page (but can either
-	// be a leaf page or an interior page)
-	page := readBTreePage(databaseFile, dbHeader, 0)
+	records := readTable(databaseFile, dbHeader, 0)
 
 	var rows []SQLiteSchemaTuple
-	switch page.Header.PageType {
-	case LEAF_TAB_PAGE:
-		for _, cell := range page.LeafTableCells {
-			// We don't yet support the cell overflowing
-			if cell.OverflowPage != nil {
-				panic("Unimplemented: cell has overflow page")
-			}
-
-			// This is separate here to serve as a reminder that we would in
-			// theory need to do some work to combine the cell payload with the
-			// overflow page(s)
-			cellPayload := cell.Payload
-
-			record := readRecord(cellPayload)
-
-			row := SQLiteSchemaTuple{
-				Type:      *record.Entries[0].Text,
-				Name:      *record.Entries[1].Text,
-				TableName: *record.Entries[2].Text,
-				RootPage:  uint32(*record.Entries[3].Number),
-				SQL:       *record.Entries[4].Text,
-			}
-			rows = append(rows, row)
+	for _, record := range records {
+		row := SQLiteSchemaTuple{
+			Type:      *record.Entries[0].Text,
+			Name:      *record.Entries[1].Text,
+			TableName: *record.Entries[2].Text,
+			RootPage:  uint32(*record.Entries[3].Number),
+			SQL:       *record.Entries[4].Text,
 		}
-	case INT_TAB_PAGE:
-		panic("Unimplemented: SQLite schema root page is an interior page")
-	default:
-		panic("SQLite schema root page is neither a leaf page nor an interior page")
+		rows = append(rows, row)
 	}
 
 	return SQLiteSchema{Rows: rows}
@@ -73,3 +43,5 @@ func (schema SQLiteSchema) TableCount() int {
 	}
 	return count
 }
+
+
---
+++ app/table.go
@@ -0,0 +1,47 @@
+package main
+
+import "os"
+
+func readTable(databaseFile *os.File, dbHeader DatabaseHeader, rootPage uint32) []Record {
+	// Read the first page, which is always a table b-tree page (but can either
+	// be a leaf page or an interior page)
+	page := readBTreePage(databaseFile, dbHeader, rootPage)
+
+	// In the simplest case, each cell on the first page corresponds to an
+	// entire record. However, if the first page is an interior page or if a
+	// cell overflows onto another page, the records for the table will be
+	// spread across multiple pages. When decoding records, we're not really
+	// interested in whether or not they span multiple pages, so we're first
+	// going to do some work to collect all of the "raw records" together (which
+	// will potentially involve reading more pages), and then we can decode
+	// these records in their entirety.
+	var rawRecords [][]byte
+
+	switch page.Header.PageType {
+	case LEAF_TAB_PAGE:
+		for _, cell := range page.LeafTableCells {
+			// TODO: support the cell overflowing
+			if cell.OverflowPage != nil {
+				panic("Unimplemented: cell has overflow page")
+			}
+
+			// This is separate here to serve as a reminder that we would in
+			// theory need to do some work to combine the cell payload with the
+			// overflow page(s)
+			cellPayload := cell.Payload
+			rawRecords = append(rawRecords, cellPayload)
+		}
+	case INT_TAB_PAGE:
+		panic("Unimplemented: SQLite schema root page is an interior page")
+	default:
+		panic("SQLite schema root page is neither a leaf page nor an interior page")
+	}
+
+	// Decode the raw records into actual records
+	var records []Record
+	for _, rawRecord := range rawRecords {
+		records = append(records, readRecord(rawRecord))
+	}
+
+	return records
+}
