f5e6e319fc75dd15c5eaf285d313f899f013677d
Lucas Graeff Buhl-Nielsen <lucasbnielsen@hotmail.com>
2024-10-21 21:34:10 +0100

    Remove unused pager test files

--- app/pager/pager_test.go
+++
@@ -1,9 +0,0 @@
-package pager_test
-
-// ReservedBytes() returns whatever was passed in when constructed
-// PageSize() returns whatever was passed in when constructed
-// Close() closes the file
-// NewPager() opens the file
-// GetPage() returns the correct page
-// GetPage() returns a page of the right size
-// Duplicate calls to GetPage() uses the cache
